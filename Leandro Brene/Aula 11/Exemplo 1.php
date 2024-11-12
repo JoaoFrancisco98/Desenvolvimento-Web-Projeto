e0762177b7b00c830a1040b82656b4e5b2c85d5f
brene01 <brene.porto@gmail.com>
2024-11-12 13:23:49 -0300

    Add files via upload

---
+++ Leandro Brene/Aula 11/Exemplo 1.php
@@ -0,0 +1,11 @@
+<?php
+echo'<h1>Bem vindo ao PHP</h1>';
+$valor = 125.45;
+$valor = $valor + 1;
+echo $valor;
+$n1 = 5;
+$n2 = 7;
+$media = ($n1 + $n2)/2;
+echo "Média é igual a => $media";
+echo 'Média é igual a => '.$media;
+?>
